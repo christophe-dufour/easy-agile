011fce6964d08178b93f3a95f2ddc82200f2ed6b
christophe <christophe@hidutyfree.com>
2014-12-03 00:20:59 +0100

    Still working on deploy

--- Gruntfile.js
+++ Gruntfile.js
@@ -321,7 +321,7 @@ module.exports = function (grunt) {
         files: [{
           expand: true,
           cwd: '<%= yeoman.dist %>',
-          src: ['*.html', 'views/{,*/}*.html'],
+          src: ['*.html', 'views/{,*/}*.html', 'templates/{,*/}*.html'],
           dest: '<%= yeoman.dist %>'
         }]
       }
@@ -360,6 +360,7 @@ module.exports = function (grunt) {
             '.htaccess',
             '*.html',
             'views/{,*/}*.html',
+            'templates/{,*/}*.html',
             'images/{,*/}*.{webp}',
             'fonts/*'
           ]
